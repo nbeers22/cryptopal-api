b16714191e928f91ebfea04542c329fbb2b95817
nbeers22 <nbeers22@gmail.com>
2019-07-29 13:21:36 -0400

    Imported new AuthRouter

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const coinsRouter = require('./coins/coinsRouter.js');
+const authRouter = require('./auth/authRouter.js');
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json()); 
 app.use('/api/coins',coinsRouter);
+app.use('/api/auth',authRouter);
 
 const errorHandler = (error,req,res,next) => {
   let response;
