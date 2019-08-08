2c201d8e501f3ca71a63af69afa0da37c13a4208
nbeers22 <nbeers22@gmail.com>
2019-08-08 17:36:34 -0400

    Created multiple unit tests to POST /api/auth/login

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -7,6 +7,7 @@ describe('Auth Endpoints', () => {
   let db;
 
   const testUsers = helpers.makeUsersArray()
+  const testUser = testUsers[0]
 
   before('make knex instance', () => {
     db = knex({
@@ -21,5 +22,62 @@ describe('Auth Endpoints', () => {
   before('cleanup', () => helpers.cleanTables(db))
 
   afterEach('cleanup', () => helpers.cleanTables(db))
+
+  describe('POST /api/auth/login', () => {
+    beforeEach('insert users', () => 
+      helpers.seedUsers( db, testUsers)
+    )
+
+    const requiredFields = ['email', 'password'];
+
+    requiredFields.forEach( field => {
+      const loginAttemptBody = {
+        email: testUser.email,
+        password: testUser.password,
+      }
+
+      it(`responds with 400 required error when ${field} is missing`, () => {
+        delete loginAttemptBody[field]
+
+        return supertest(app)
+          .post("/api/auth/login")
+          .send(loginAttemptBody)
+          .expect(400, {
+            error: `Missing ${field} in request`
+          })
+      })
+    });
+
+    it(`responds with 400 error when user email is not found in database`, () => { 
+      const fakeUser = {
+        email: 'fake.email@mail.com',
+        password: 'fakePassword'
+      }
+
+      return supertest(app)
+        .post("/api/auth/login")
+        .send(fakeUser)
+        .expect(400, {
+          error: `Incorrect email and/or password`
+        })
+    })
+    
+    it(`responds with 400 error when user email found but password is wrong`, () => { 
+      const userWithBadPassword = {
+        email: testUser.email,
+        password: 'fakePassword'
+      }
+
+      return supertest(app)
+        .post("/api/auth/login")
+        .send(userWithBadPassword)
+        .expect(400, {
+          error: `Incorrect email and/or password`
+        })
+    })
+  })
+  
+  
+  
 
 });
