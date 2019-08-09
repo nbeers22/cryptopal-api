84d03ad31a2ac5b312c1b220e7572c0e8761aaf8
nbeers22 <nbeers22@gmail.com>
2019-08-09 12:08:15 -0400

    Added different or clause to jwt_secret token phrase

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 4000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://cryptopal@localhost/cryptopal',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  JWT_SECRET: process.env.JWT_SECRET || 'jsgafuyafhiuashfniuyeradhlversdgbsfv',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '24h',
 }
